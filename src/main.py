02d1e8185a808f83c8a5c8689ad2145c34fe15ee
EthanColbert8 <emcolbert2@gmail.com>
2025-06-04 22:34:54 -0400

    add folder copy functionality

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
-from . import blockparser
+import os
+import shutil
+
+def copy_tree(src: str, dest: str):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    os.mkdir(dest)
+    
+    for item in os.listdir(src):
+        src_path = os.path.join(src, item)
+        dest_path = os.path.join(dest, item)
+        
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dest_path)
+        else:
+            copy_tree(src_path, dest_path)
 
 if (__name__ == "__main__"):
-    markdown1 = "## This is a heading."
-    markdown2 = "```Here's some\ncode()\nwith multiple lines\nand symbols[]```"
-    #markdown2 = "- This is an\n- unordered list"
-
-    print(blockparser.block_to_blocktype(markdown1))
-    print(blockparser.block_to_blocktype(markdown2))
+    copy_tree("static", "public")
